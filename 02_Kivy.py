7f7205d686d7fd0bb8cf47067104801c8807fb06
balaji <balajikrishnan00@gmail.com>
2021-07-09 14:32:55 +0300

    kivy

---
+++ 02_Kivy.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MYGrid(GridLayout):
+    def __init__(self,**kwargs):
+        super(MYGrid, self).__init__(**kwargs)
+        self.cols=2
+
+        self.Name=TextInput(multiline=False)
+        self.Color=TextInput(multiline=False)
+        self.Pizza=TextInput(multiline=False)
+
+        self.add_widget(Label(text='name'))
+        self.add_widget(self.Name)
+
+        self.add_widget(Label(text='color'))
+        self.add_widget(self.Color)
+
+        self.add_widget(Label(text='pizza'))
+        self.add_widget(self.Pizza)
+
+        self.btn=Button(text='Submit')
+        self.btn.bind(on_press=root.press) # MYApp.press
+        self.add_widget(self.btn)
+
+
+class MYApp(App):
+    def press(self,i):
+        print('Hello Kivy',i)
+
+    def build(self):
+
+        return MYGrid()
+if __name__ == '__main__':
+
+    root=MYApp()
+    root.run()
